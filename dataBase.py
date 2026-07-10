4a9304e87b03593574d53552887745c43100e997
brian <ivan.rios2010@gmail.com>
2026-07-09 22:20:20 -0300

    cambios menores

--- dataBase.py
+++ dataBase.py
@@ -1,33 +1,4 @@
 import psycopg2
-
-conn = psycopg2.connect(
-    dbname = "trading_bot",
-    user = "postgres",
-    password = "postgres",
-    host="localhost",
-    port="5432"
-) 
-cur = conn.cursor()
-
-cur.execute(
-    "SELECT * FROM operaciones"
-    )
-#to insert data we can use
-#cur.execute("INSERT INTO operaciones (moneda, monto, precio, tipo) VALUES (%s,%s,%s,%s)",("ETHUSDT",0.004, 2450.0, True) # T compra, F venta) 
-print(cur.fetchall())
-
-conn.commit()
-cur.close()
-conn.close()
-
-
-
-# aca habria que crear una base de datos, y guardar 
-# moneda, monto, precio de la moneda, venta/compra,dia y hora
-
-# despues habria que crear la api y que devuelva todas lascompras y las ventas que se hicieron 
-# basicamente un historial.
-
 import os
 from pathlib import Path
 from dotenv import load_dotenv
